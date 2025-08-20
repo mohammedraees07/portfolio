4f317cd41d7f5e30d86e6d980653df0145f08b73
Mohammed Raees <mohammedraees004@gmail.com>
2025-08-20 22:15:27 +0530

    216Update

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -67,7 +67,7 @@ const Contact = () => {
             <div className="contact__card">
               <i className="bx bxl-messenger contact__card-icon"></i>
               <h3 className="contact__card-title">Messenger</h3>
-              <span className="contact__card-data">user.fb123</span>
+              <span className="contact__card-data"></span>
               <a href="https://m.me/user.fb123" className="contact__button">
                 Write me{" "}
                 <i className="bx bx-right-arrow-alt contact__button-icon"></i>
