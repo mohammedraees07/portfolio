623f8241ab330890bf370f170f0a5055532f045e
Mohammed Raees <mohammedraees004@gmail.com>
2025-08-25 23:45:00 +0530

    resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './about.css';
 import AboutImg from '../../assets/ProfileAbout.jpg';
-import CV from '../../assets/Cv.pdf';
+import CV from '../../assets/Resume.pdf';
 import Info from './Info';
 
 function About() {
